2fe0adf9f1ffa5841c330b162c768564aadc2571
SahdevTomar <133491812+SahdevTomar@users.noreply.github.com>
2025-10-29 01:15:50 +0530

    Create controcontroller.php

---
+++ Edustream/Backend/controcontroller.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json(['message' => 'User registered successfully', 'user' => $user]);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Invalid Credentials'], 401);
+        }
+
+        return response()->json(['token' => $token]);
+    }
+
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+}
